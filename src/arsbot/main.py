825a12f7014c4111537ac8223353b49c9ee04989
Matt Ullman <static@staticfox.net>
2024-12-25 20:00:43 -0500

    Add initdb command to arsbot

--- src/arsbot/main.py
+++ src/arsbot/main.py
@@ -7,24 +7,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from .core.config import validate_config
+from .core.db import bot_session
 from .core.logging import setup_loggers
 from .discord.run import runbot
+from .models.base import BotBase
 
 log = logging.getLogger("arsbot")
 
 
 parser = argparse.ArgumentParser(
     prog="arsbot",
-    description="Runs ARS bot. No arguments currently supported.",
+    description="Runs ARS bot.",
 )
+parser.add_argument("--action", choices=["runbot", "initdb"], default="runbot", help="Which action to run")
+
+
+def initdb():
+    with bot_session() as session:
+        BotBase.metadata.create_all(session.bind)
 
 
 def main():
-    _args = parser.parse_args()
+    args = parser.parse_args()
 
     setup_loggers()
     validate_config()
 
-    runbot()
+    if args.action == "runbot":
+        runbot()
+    elif args.action == "initdb":
+        initdb()
 
     log.debug("main done!!")
